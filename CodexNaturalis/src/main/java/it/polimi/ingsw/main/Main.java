0183645366032d1b996ef0ee865242b8d6198ce4
alemeacci <alessandro.meacci@mail.polimi.it>
2024-03-19 17:53:14 +0100

    Refactored Main class

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/main/Main.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.main;
+
+import it.polimi.ingsw.enumerations.Marker;
+import it.polimi.ingsw.model.Player;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Player test = new Player("G", Marker.RED);
+
+    }
+}
