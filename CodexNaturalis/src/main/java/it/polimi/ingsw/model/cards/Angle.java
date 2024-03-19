696b39566617b16443418ca4469660d16fab7ac5
alemeacci <alessandro.meacci@mail.polimi.it>
2024-03-19 16:50:34 +0100

    Finished Angle class and added unlinkedCardException

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Angle.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Angle.java
@@ -2,18 +2,49 @@ package it.polimi.ingsw.model.cards;
 
 import it.polimi.ingsw.enumerations.AngleStatus;
 import it.polimi.ingsw.enumerations.Resource;
+import it.polimi.ingsw.exceptions.unlinkedCardException;
+
 
 public class Angle {
     private final boolean playable;
     private AngleStatus angleStatus;
     private final Resource resource;
     private Angle linkedAngle;
+    private final CardWithAngles ownerCard;
 
-    public Angle(boolean playable, Resource resource)
+    public Angle(boolean playable, Resource resource, CardWithAngles ownerCard)
     {
         this.playable = playable;
         this.resource = resource;
         this.angleStatus = AngleStatus.UNLINKED;
+        this.ownerCard = ownerCard;
+    }
+    public boolean isPlayable()
+    {
+        return playable;
+    }
+    public AngleStatus getAngleStatus()
+    {
+        return angleStatus;
+    }
+    public Resource getResource() {
+        return resource;
     }
 
+    public void setAngleStatus(AngleStatus angleStatus) {
+        this.angleStatus = angleStatus;
+    }
+    public void setLinkedAngle(Angle linkedAngle)
+    {
+        this.linkedAngle = linkedAngle;
+    }
+    public Angle getLinkedAngle() throws unlinkedCardException {
+        if(angleStatus == AngleStatus.UNLINKED)
+            throw new unlinkedCardException();
+        return linkedAngle;
+    }
+    public CardWithAngles getOwnerCard()
+    {
+        return ownerCard;
+    }
 }
