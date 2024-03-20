ab0bcb1eab139d9f69563532d020777174d52f03
andreeapaganelli <160610286+andreeapaganelli@users.noreply.github.com>
2024-03-20 15:24:20 +0100

    Started to create the PlayerHand class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerHand.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerHand.java
@@ -2,9 +2,22 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.enumerations.Draw;
 import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.PlayableCard;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class PlayerHand {
-    //list of PlayerCard --> the Hand of the player
+    private List<PlayableCard> cardsInHand;
+    private DrawingField drawingField;
+    private PlayerField playerField;
+
+    public PlayerHand(List<PlayableCard> cardsInHand) {
+        this.cardsInHand = new LinkedList<PlayableCard>();
+        try {
+            this.cardsInHand.addAll(cardsInHand);
+        } catch (NullPointerException e) { }
+    }
 
     public void chooseCardToDraw(Draw draw, Deck deck) {
 
