0129cb241e7696645fec208ba635f1fdf9076f38
Christian <christian.mariano@mail.polimi.it>
2024-03-26 16:41:39 +0100

    GoldCard JavaDoc

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/GoldCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/GoldCard.java
@@ -9,12 +9,28 @@ import it.polimi.ingsw.exceptions.InvalidConstructorDataException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Class that represents a gold card
+ */
 public class GoldCard extends ResourceCard {
 
     private final ArrayList<Resource> requirements;
     private final GoldPointCondition goldPointCondition;
 
     //if the card has no requirements, controller has to pass requirements with NONE value
+
+    /**
+     * Constructor
+     * @param cardId id associated to the card
+     * @param currentSide indicates one of the two side of the card (front or back)
+     * @param centralResource list of the resources in the middle of the card
+     * @param angles maps the position of the angle into the angle object
+     * @param cardColor one of the four colors, each associated with a resource
+     * @param requirements list of the resources needed to play a card
+     * @param goldPointCondition condition to satisfied to obtain the points given by the card
+     * @param points points given by a card by positioning it
+     * @throws InvalidConstructorDataException if an invalid parameter is given
+     */
     public GoldCard(int cardId, Side currentSide, ArrayList<Resource> centralResource, HashMap<AngleOrientation, Angle> angles, Resource cardColor, ArrayList<Resource> requirements, GoldPointCondition goldPointCondition, int points) throws InvalidConstructorDataException {
         super(cardId, currentSide, centralResource, angles, cardColor, points);
         try {
@@ -27,14 +43,21 @@ public class GoldCard extends ResourceCard {
         }
     }
 
+    /**
+     *
+     * @return the list of the resources needed to play a card
+     */
     public ArrayList<Resource> getRequirements()
     {
         return new ArrayList<Resource>(requirements);
     }
+
+    /**
+     *
+     * @return the condition to satisfied to obtain the points given by the card
+     */
     public GoldPointCondition getGoldPointCondition()
     {
         return goldPointCondition;
     }
-
-
 }
