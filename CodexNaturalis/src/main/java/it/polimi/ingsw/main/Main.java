5ea8c206232de0b47eb372040282e1752704cea1
Simone1Martini <simone1.martini@mail.polimi.it>
2024-04-09 17:05:52 +0200

    New UML of the model

--- CodexNaturalis/src/main/java/it/polimi/ingsw/main/Main.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/main/Main.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.main;
 import it.polimi.ingsw.enumerations.Marker;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.GoldCard;
 
 import java.util.HashMap;
 
