e611fcde7dd5fe628c94f8dbde7b66befc3ba46d
Simone1Martini <sicmartini01@gmail.com>
2024-04-11 13:54:56 +0200

    First test for the player class, unable to run the test because Maven cannot find project classes

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -1,64 +1,28 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.enumerations.Marker;
+import it.polimi.ingsw.exceptions.CardTypeMismatchException;
+import it.polimi.ingsw.exceptions.DeckIsEmptyException;
+import it.polimi.ingsw.exceptions.InvalidConstructorDataException;
+import it.polimi.ingsw.model.cards.PlayableCard;
+import it.polimi.ingsw.model.cards.TripleObjectiveCard;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
     @Test
-    void playCard() {
-    }
-
-    @Test
-    void getUsername() {
-    }
-
-    @Test
-    void getIsTurn() {
-    }
-
-    @Test
-    void setIsTurn() {
-    }
-
-    @Test
-    void getIsFirst() {
-    }
-
-    @Test
-    void setIsFirst() {
-    }
-
-    @Test
-    void getMarker() {
-    }
-
-    @Test
-    void getResourceAmount() {
-    }
-
-    @Test
-    void updateResourceAmount() {
-    }
-
-    @Test
-    void getPoints() {
-    }
-
-    @Test
-    void addPoints() {
-    }
-
-    @Test
-    void chooseGoldCardToDraw() {
-    }
-
-    @Test
-    void chooseResourceCardToDraw() {
-    }
-
-    @Test
-    void getPlayerField() {
+    void shouldReturnUsername() throws InvalidConstructorDataException, CardTypeMismatchException, DeckIsEmptyException {
+        List<PlayableCard> li = new ArrayList<>();
+        Deck d1 = new Deck(li);
+        Deck d2 = new Deck(li);
+        DrawingField d = new DrawingField(d1, d2);
+        List<TripleObjectiveCard> l = new ArrayList<>();
+        Game test = new Game(123, d, l);
+        Player p1 = new Player("test", Marker.BLACK, test);
     }
 }
