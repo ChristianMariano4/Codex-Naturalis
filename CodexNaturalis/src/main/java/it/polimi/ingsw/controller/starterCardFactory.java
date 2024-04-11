90c6f5603c88cbef3c050894813037e0a863b2d1
alemeacci <alessandro.meacci@mail.polimi.it>
2024-04-11 18:18:41 +0200

    Changed json starterCardFactory parser to use relative path instead of absolute path

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/starterCardFactory.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/starterCardFactory.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.enumerations.AngleOrientation;
 import it.polimi.ingsw.enumerations.Resource;
 import it.polimi.ingsw.model.cards.StarterCard;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -12,8 +13,7 @@ import java.io.Reader;
 public class starterCardFactory {
     public static void main(String[] args) {
         Gson gson = new Gson();
-
-        try(Reader reader = new FileReader("C:\\Users\\hp\\IdeaProjects\\ing-sw-2024-paganelli-mariano-meacci-martini\\CodexNaturalis\\resources\\starterCards.json")) {
+        try(Reader reader = new FileReader("CodexNaturalis/resources/starterCards.json")) {
             StarterCard starterCard = gson.fromJson(reader, StarterCard.class);
             System.out.println(starterCard.getAngle(AngleOrientation.TOPRIGHT).getResource());
         } catch (IOException e) {
--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/starterCardjson.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/starterCardjson.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class startedCardjson {
+public class starterCardjson {
     public static void main(String[] args) throws InvalidConstructorDataException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
