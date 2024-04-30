5e0ab6840cf268a0d510c03e2a8e50ac9f9ac879
alemeacci <alessandro.meacci@mail.polimi.it>
2024-04-30 15:24:56 +0200

    Added AngleOrientation enum methods for LShaped Objective Card point calculation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/enumerations/AngleOrientation.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/enumerations/AngleOrientation.java
@@ -24,5 +24,40 @@ public enum AngleOrientation {
             case NONE -> 0;
         };
     }
+    public int mapEnumToXLShaped()
+    {
+        int xValue;
+        return switch (this) {
+            case TOPRIGHT, BOTTOMLEFT -> {
+                xValue = 1;
+                yield xValue;
+            }
+            case TOPLEFT, BOTTOMRIGHT -> {
+                xValue = -1;
+                yield xValue;
+            }
+            case NONE -> 0;
+        };
+    }
+    public int[] mapEnumToYLShaped()
+    {
+        int[] yValues = new int[2];
+        return switch(this)
+        {
+            case TOPRIGHT, TOPLEFT-> {
+                yValues[0] = 1;
+                yValues[1] = 2;
+                yield yValues;
+            }
+            case BOTTOMRIGHT, BOTTOMLEFT-> {
+                yValues[0] = -1;
+                yValues[1] = -2;
+                yield yValues;
+            }
+            case NONE-> {
+                yield yValues;
+            }
+        };
+    }
 
 }
