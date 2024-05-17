28de1edb82ab48875a2cc02c6a0a20b9f95f4c85
Simone1Martini <simone1.martini@mail.polimi.it>
2024-05-17 14:31:40 +0200

    created scoreboardScreen fxml file

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/SoundController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/SoundController.java
@@ -8,18 +8,15 @@ import java.net.URISyntaxException;
 
 public class SoundController extends GUIController {
 
-    private MediaView mediaView;
-
     public void buttonClick() {
         try {
             String fileName = getClass().getResource("/sounds/buttonClick.wav").toURI().toString();
             Media media = new Media(fileName);
             MediaPlayer player = new MediaPlayer(media);
-            mediaView = new MediaView();
-            mediaView.setMediaPlayer(player);
+            player.setVolume(0.5);
+            player.play();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-        mediaView.getMediaPlayer().play();
     }
 }
