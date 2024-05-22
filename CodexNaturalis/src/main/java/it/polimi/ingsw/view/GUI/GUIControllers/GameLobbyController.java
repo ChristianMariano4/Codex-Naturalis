16feb24f2cba2ff25c7c4161a0981b34cabfcf9a
Simone1Martini <simone1.martini@mail.polimi.it>
2024-05-22 14:31:25 +0200

    Game lobby screen changes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/GameLobbyController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/GameLobbyController.java
@@ -3,20 +3,42 @@ package it.polimi.ingsw.view.GUI.GUIControllers;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameLobbyController extends GUIController {
     public TabPane rulebookTabPane;
+    public Pane playersPane;
+    public Pane playersPane1;
     private boolean isInRulebook = true;
-    public Button rulebookButton;
+    public Text p1name;
+    public Text p2name;
+    public Text p3name;
+    public Text p4name;
 
     public void rulebookButton() {
         if(isInRulebook) {
             rulebookTabPane.setDisable(false);
             rulebookTabPane.setVisible(true);
+
+            playersPane.setDisable(true);
+            playersPane.setVisible(false);
+            playersPane1.setDisable(true);
+            playersPane1.setVisible(false);
+
             isInRulebook = false;
         } else  {
             rulebookTabPane.setDisable(true);
             rulebookTabPane.setVisible(false);
+
+            playersPane.setDisable(false);
+            playersPane.setVisible(true);
+            playersPane1.setDisable(false);
+            playersPane1.setVisible(true);
+
             isInRulebook = true;
         }
 
