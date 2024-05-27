ee3a3404a0fa2ced8d6a5878864ab421320396f6
Simone1Martini <simone1.martini@mail.polimi.it>
2024-05-27 15:19:22 +0200

    Game Lobby changes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/GameLobbyController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GUIControllers/GameLobbyController.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.view.GUI.GUIControllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class GameLobbyController extends GUIController {
     public TabPane rulebookTabPane;
+    public Pane rulebookPane;
     public Pane playersPane;
     public Pane playersPane1;
     private boolean isInRulebook = true;
@@ -18,11 +20,18 @@ public class GameLobbyController extends GUIController {
     public Text p2name;
     public Text p3name;
     public Text p4name;
+    public Pane p1;
+    public Pane p2;
+    public Pane p3;
+    public Pane p4;
+    public Button nextButton;
+    public Button backButton;
+    private Integer playerNum = 1;
 
     public void rulebookButton() {
         if(isInRulebook) {
-            rulebookTabPane.setDisable(false);
-            rulebookTabPane.setVisible(true);
+            rulebookPane.setDisable(false);
+            rulebookPane.setVisible(true);
 
             playersPane.setDisable(true);
             playersPane.setVisible(false);
@@ -31,8 +40,8 @@ public class GameLobbyController extends GUIController {
 
             isInRulebook = false;
         } else  {
-            rulebookTabPane.setDisable(true);
-            rulebookTabPane.setVisible(false);
+            rulebookPane.setDisable(true);
+            rulebookPane.setVisible(false);
 
             playersPane.setDisable(false);
             playersPane.setVisible(true);
@@ -41,6 +50,45 @@ public class GameLobbyController extends GUIController {
 
             isInRulebook = true;
         }
+    }
+    @FXML
+    public void nextRulebookSlide() {
+        rulebookTabPane.getSelectionModel().select(rulebookTabPane.getSelectionModel().getSelectedIndex()+1);
+        nextButton.disableProperty().bind(rulebookTabPane.getSelectionModel().selectedIndexProperty().greaterThanOrEqualTo(10));
+    }
+    @FXML
+    public void previousRulebookSlide() {
+        rulebookTabPane.getSelectionModel().select(rulebookTabPane.getSelectionModel().getSelectedIndex()-1);
+        backButton.disableProperty().bind(rulebookTabPane.getSelectionModel().selectedIndexProperty().lessThanOrEqualTo(0));
+    }
+    public void showPlayerPane() {
+        switch (playerNum) {
+            case 1:
+                p1.setDisable(false);
+                p1.setVisible(true);
+                p1name.setText("fdhsfhskdj");
+                break;
+            case 2:
+                p2.setDisable(false);
+                p2.setVisible(true);
+                p2name.setText("fdhsfhskdj");
+                break;
+            case 3:
+                p3.setDisable(false);
+                p3.setVisible(true);
+                p3name.setText("fdhsfhskdj");
+                break;
+            case 4:
+                p4.setDisable(false);
+                p4.setVisible(true);
+                p4name.setText("fdhsfhskdj");
+                break;
+        }
+        playerNum++;
+    }
+    @FXML
+    public void setReadyButton() {
 
     }
+
 }
