00669e75354decd30aaad9dfec1cd6f433b47fe2
Christian <christian.mariano@mail.polimi.it>
2024-06-24 16:36:19 +0200

    tested CardInfo

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/cards/CardInfoTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/cards/CardInfoTest.java
@@ -11,16 +11,80 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CardInfoTest {
     private CardInfo goldCard;
+    private CardInfo goldCardFiveResources;
+    private CardInfo goldCardThreeResources;
+    private CardInfo goldCardAngle;
+    private CardInfo goldCardZeroResources;
     private CardInfo resourceCard;
     private CardInfo resourceObjectiveCard;
-    private CardInfo positionalObjectiveCard;
+    private CardInfo positionalObjectiveCardDiagonal;
+    private CardInfo positionalObjectiveCardLShaped;
+
 
     @BeforeEach
     void setUp() {
         goldCard = new CardInfo(CardType.GOLD, new ArrayList<>(Arrays.asList(Resource.FUNGI, Resource.FUNGI, Resource.ANIMAL)), GoldPointCondition.MANUSCRIPT);
+        goldCardFiveResources = new CardInfo(CardType.GOLD, new ArrayList<>(Arrays.asList(Resource.FUNGI, Resource.FUNGI, Resource.FUNGI, Resource.FUNGI, Resource.FUNGI)), GoldPointCondition.NONE);
+        goldCardThreeResources = new CardInfo(CardType.GOLD, new ArrayList<>(Arrays.asList(Resource.FUNGI, Resource.FUNGI, Resource.FUNGI)), GoldPointCondition.NONE);
+        goldCardAngle = new CardInfo(CardType.GOLD, new ArrayList<>(Arrays.asList(Resource.FUNGI, Resource.FUNGI, Resource.ANIMAL)), GoldPointCondition.ANGLE);
+        goldCardZeroResources = new CardInfo(CardType.GOLD, new ArrayList<>(), GoldPointCondition.NONE);
         resourceCard = new CardInfo(CardType.RESOURCE);
         resourceObjectiveCard = new CardInfo(CardType.RESOURCEOBJECTIVE, Resource.INSECT);
-        positionalObjectiveCard = new CardInfo(CardType.POSITIONALOBJECTIVE, Resource.INSECT, AngleOrientation.TOPLEFT, PositionalType.DIAGONAL);
+        positionalObjectiveCardDiagonal = new CardInfo(CardType.POSITIONALOBJECTIVE, Resource.INSECT, AngleOrientation.TOPLEFT, PositionalType.DIAGONAL);
+        positionalObjectiveCardLShaped = new CardInfo(CardType.POSITIONALOBJECTIVE, Resource.INSECT, AngleOrientation.TOPLEFT, PositionalType.LSHAPED);
+    }
+
+    @Test
+    void goldCardShouldReturnCorrectPoints() {
+        assertEquals(1, goldCard.getPoints());
+    }
+
+    @Test
+    void goldCardThreeResourcesShouldReturnCorrectPoints() {
+        assertEquals(3, goldCardThreeResources.getPoints());
+    }
+
+    @Test
+    void goldCardFiveResourcesShouldReturnCorrectPoints() {
+        assertEquals(5, goldCardFiveResources.getPoints());
+    }
+
+    @Test
+    void goldCardAngleShouldReturnCorrectPoints() {
+        assertEquals(2, goldCardAngle.getPoints());
+    }
+
+    @Test
+    void goldCardZeroResourcesShouldReturnCorrectPoints() {
+        assertEquals(0, goldCardZeroResources.getPoints());
+    }
+
+    @Test
+    void resourceCardShouldReturnCorrectPoints() {
+        assertEquals(2, resourceCard.getPoints());
+    }
+
+    @Test
+    void resourceObjectiveCardShouldReturnCorrectPoints() {
+        assertEquals(2, resourceObjectiveCard.getPoints());
+    }
+
+    @Test
+    void positionalObjectiveCardShouldReturnCorrectPoints() {
+        assertEquals(2, positionalObjectiveCardDiagonal.getPoints());
+    }
+
+    @Test
+    void positionalObjectiveCardLShapedShouldReturnCorrectPoints() {
+        assertEquals(3, positionalObjectiveCardLShaped.getPoints());
+    }
+
+    @Test
+    void cardShouldReturnCorrectType() {
+        assertEquals("Gold", goldCard.getCardTypeString());
+        assertEquals("Resource", resourceCard.getCardTypeString());
+        assertEquals("Objective", resourceObjectiveCard.getCardTypeString());
+        assertEquals("Objective", positionalObjectiveCardDiagonal.getCardTypeString());
     }
 
     @Test
@@ -58,12 +122,12 @@ class CardInfoTest {
 
     @Test
     void positionalObjectiveCardShouldReturnCorrectAttributes() {
-        assertEquals(CardType.POSITIONALOBJECTIVE, positionalObjectiveCard.getCardType());
-        assertTrue(positionalObjectiveCard.getRequirements().isEmpty());
-        assertEquals(GoldPointCondition.NONE, positionalObjectiveCard.getGoldPointCondition());
-        assertEquals(Resource.INSECT, positionalObjectiveCard.getCardColor());
-        assertEquals(AngleOrientation.TOPLEFT, positionalObjectiveCard.getOrientation());
-        assertEquals(PositionalType.DIAGONAL, positionalObjectiveCard.getPositionalType());
-        assertEquals(Resource.NONE, positionalObjectiveCard.getCardResource());
+        assertEquals(CardType.POSITIONALOBJECTIVE, positionalObjectiveCardDiagonal.getCardType());
+        assertTrue(positionalObjectiveCardDiagonal.getRequirements().isEmpty());
+        assertEquals(GoldPointCondition.NONE, positionalObjectiveCardDiagonal.getGoldPointCondition());
+        assertEquals(Resource.INSECT, positionalObjectiveCardDiagonal.getCardColor());
+        assertEquals(AngleOrientation.TOPLEFT, positionalObjectiveCardDiagonal.getOrientation());
+        assertEquals(PositionalType.DIAGONAL, positionalObjectiveCardDiagonal.getPositionalType());
+        assertEquals(Resource.NONE, positionalObjectiveCardDiagonal.getCardResource());
     }
 }
